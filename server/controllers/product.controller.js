e3bd02907b4412ae003666f94336aa90f258161a
Amit Kushwaha <amitcoding56@gmail.com>
2025-04-04 07:47:24 +0530

    all controller write

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,8 +1,16 @@
+import mongoose from "mongoose";
 import productModel from "../models/product.models.js";
+
+// Get Products
 
 export const getProducts = async (req, res) => {
   try {
-  } catch (error) {}
+    const products = await productModel.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 };
 
 // Product Creation
@@ -26,15 +34,41 @@ export const createProduct = async (req, res) => {
   }
 };
 
+// Update LOGIC
+
 export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
   try {
-  } catch (error) {}
+    const updateProduct = await productModel.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.log("error in updating product", error.message);
+    res.status(500).json({ success: false, message: "server error" });
+  }
 };
+
+// Delete Logic
 
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid Product id" });
+  }
 
   try {
+    await productModel.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
   } catch (error) {
     console.log("Error in deleteing Product", error.message);
     res.status(500).json({ success: false, message: "server error" });
